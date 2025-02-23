0ce4e5fc51ee62a256e5f7715760436544c22fab
ZR-Morris-Wang <ppmmlzr@gmail.com>
2025-02-23 13:40:42 +0800

    [Checkpoint] Conditional Rendering based on URL

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [MatSidenavModule, MatListModule, MatIconModule],
+  imports: [MatSidenavModule, MatListModule, MatIconModule, RouterOutlet, RouterLink],
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss'],
 })
